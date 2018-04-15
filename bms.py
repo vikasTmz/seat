e9f33f3371898777161267a3666bfdb82db1fc41
Vikas Thamizharasan <vikas.thamizharasan@students.iiit.ac.in>
2018-04-15 21:38:48 +0530

    Update bms.py

--- bms.py
+++ bms.py
@@ -82,3 +82,4 @@ if __name__ == "__main__":
 		bookticket(SHOWS["sujana"], "B_" + str(i).zfill(1) + "_13", "//*[@id=\"pop_5\"]")
 
 
+# wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux64.tar.gz
